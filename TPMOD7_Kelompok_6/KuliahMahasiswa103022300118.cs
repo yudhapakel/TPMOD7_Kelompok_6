52edcfdb876df60a2fe6126fb55e45088deb8b78
Rizqie Ahmad <rizqie@rectmedia.com>
2025-04-08 00:16:44 +0700

    menambahkan class KuliahMahasiswa103022300118

---
+++ TPMOD7_Kelompok_6/KuliahMahasiswa103022300118.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
+
+namespace TPMOD7_Kelompok_6
+{
+    public class KuliahMahasiswa103022300118
+    {
+        public class MataKuliah
+        {
+            public string code { get; set; }
+            public string name { get; set; }
+        }
+
+        public class Kuliah
+        {
+            public List<MataKuliah> course { get; set; }
+        }
+
+        public static void ReadJSON()
+        {
+            string json = File.ReadAllText("tp7_2_103022300118.json");
+            Kuliah data = JsonSerializer.Deserialize<Kuliah>(json);
+
+            Console.WriteLine("Daftar mata kuliah yang diambil:");
+            int i = 1;
+            foreach (var mk in data.course)
+            {
+                Console.WriteLine($"MK {i} {mk.code} - {mk.name}");
+                i++;
+            }
+        }
+    }
+}
