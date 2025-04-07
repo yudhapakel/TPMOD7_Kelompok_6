4497866d27806395743190af16a05b9be2fc71df
yudhapakel <yudahp23@gmail.com>
2025-04-07 11:19:29 +0700

    menambahkan class DataMahasiswa103022300010

---
+++ TPMOD7_Kelompok_6/DataMahasiswa103022300010.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+public class DataMahasiswa12345678
+{
+    public class Mahasiswa
+    {
+        public string Nama { get; set; }
+        public string Nim { get; set; }
+        public string Fakultas { get; set; }
+    }
+
+    public static void ReadJSON()
+    {
+        string json = File.ReadAllText("tp7_1_12345678.json");
+        Mahasiswa mhs = JsonSerializer.Deserialize<Mahasiswa>(json);
+        Console.WriteLine($"Nama {mhs.Nama} dengan nim {mhs.Nim} dari fakultas {mhs.Fakultas}");
+    }
+}
