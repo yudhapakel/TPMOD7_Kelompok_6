c6f8128b81a4c200c872b816c133f119d2ea3c4c
NurIlmiM <mufidahnurilmi@gmail.com>
2025-04-07 13:45:13 +0700

    menambahkan class DataMahasiswa103022300005

---
+++ TPMOD7_Kelompok_6/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TPMOD7_Kelompok_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataMahasiswa103022300005.ReadJSON();
+        }
+    }
+}
